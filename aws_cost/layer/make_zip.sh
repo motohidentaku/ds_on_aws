2b3c3fa70c1cfdd1f1edbf13893b9fc7990f1c9b
EC2 Default User <ec2-user@ip-172-31-14-149.ap-northeast-1.compute.internal>
2022-03-16 05:53:29 +0000

    add layer zip file

--- aws_cost/layer/make_zip.sh
+++ aws_cost/layer/make_zip.sh
@@ -1,4 +1,6 @@
-sudo yum update
+#!/bin/bash
+
+sudo yum -y update
 pip3 install selenium==3.141.0 -t python/lib/python3.7/site-packages
 curl -SL https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-37/stable-headless-chromium-amazonlinux-2017-03.zip > headless-chromium.zip
 unzip -o headless-chromium.zip -d .
